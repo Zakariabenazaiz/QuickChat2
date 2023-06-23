61dc6915669488a5ad81f847e1cf4e28f5c66a02
Zakariabenazaiz <76950392+Zakariabenazaiz@users.noreply.github.com>
2023-06-23 17:58:52 +0100

    Add files via upload

---
+++ Whatsapp Opener.py
@@ -0,0 +1,29 @@
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
+import urllib.parse
+
+TOKEN = '6133105355:AAGgbpAkJRViH2UXHQy_ruvhOkfaVxPLTXc'
+
+def start(update, context):
+    welcome_message = "Welcome to the WhatsApp bot! Please enter a phone number with the international code."
+    update.message.reply_text(welcome_message)
+
+def handle_phone_number(update, context):
+    phone_number = update.message.text
+    formatted_number = urllib.parse.quote(phone_number, safe='')
+    formatted_number = formatted_number.replace(' ', '')  # Remove any spaces in the phone number
+    whatsapp_url = f"https://api.whatsapp.com/send?phone={formatted_number}&text=?"
+    update.message.reply_text(f"Click the link below to open the chat in WhatsApp:\n\n{whatsapp_url}")
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(MessageHandler(Filters.text & (~Filters.command), handle_phone_number))
+    dp.add_handler(MessageHandler(Filters.command, start))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
